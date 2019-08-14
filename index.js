ea60e67870db38342537166e863270f8bf3eb582
Jose Nicdao <jonicdao@gmail.com>
2019-08-14 18:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
  function updateObjectWithKeyAndValue(obj, key, value) {
-    obj[key] = value
+   obj[key] = value
    return  Object.assign ({}, obj, {key: value})
 }
